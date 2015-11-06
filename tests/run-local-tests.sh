5dd1a5a15b628c7a0db158788990a33426b6d2c9
Felix Fennell <felnne@bas.ac.uk>
2015-11-06 16:19:55 +0000

    Tidying up

--- tests/run-local-tests.sh
+++ tests/run-local-tests.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 # Check syntax:
-ansible-playbook -i provisioning/site-test.yml --syntax-check
+ansible-playbook provisioning/site-test.yml --syntax-check
 
 # Apply playbook:
-ansible-playbook -i provisioning/site-test.yml
+ansible-playbook provisioning/site-test.yml
 
 # Apply again to check idempotency:
-ansible-playbook -i provisioning/site-test.yml
+ansible-playbook provisioning/site-test.yml
